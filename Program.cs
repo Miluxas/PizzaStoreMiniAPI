70c702f6ec6561f72c5186347ce39733da5c029d
milad ashoori <milad.ashoori@gmail.com>
2022-01-22 04:14:09 -0800

    add Db for Pizzas

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("V1",new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title ="PizzaStore Mini API",
+        Description ="Its a mini api in .net core",
+        Version ="v1"
+    });
 
+});
+var app = builder.Build();
+if(app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json","PizzaStore Mini API V1");
+});
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
