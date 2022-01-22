ed99d8564235012dd741da9b3d7db67310d95fa4
milad ashoori <milad.ashoori@gmail.com>
2022-01-22 06:45:16 -0800

    add in memory db

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-using PizzaStoreMiniAPI.DB;
+using Microsoft.EntityFrameworkCore;
+using PizzaStoreMiniAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDb>(OptionsBuilderConfigurationExtensions=>OptionsBuilderConfigurationExtensions.UseInMemoryDatabase("items"));
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1",new Microsoft.OpenApi.Models.OpenApiInfo
@@ -21,10 +24,30 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json","PizzaStore Mini API V1");
 });
 
-app.MapGet("/pizzas/{id}",(int id)=>PizzaStoreMiniAPI.DB.PizzaDB.GetPizza(id));
-app.MapGet("/pizzas",()=>PizzaStoreMiniAPI.DB.PizzaDB.GetPizzas);
-app.MapPost("/pizzas",(Pizza pizaa)=>PizzaStoreMiniAPI.DB.PizzaDB.CreatePizza(pizaa));
-app.MapPut("/pizzas",(Pizza pizaa)=>PizzaStoreMiniAPI.DB.PizzaDB.UpdatePizza(pizaa));
-app.MapDelete("/pizzas/{id}",(int id)=>PizzaStoreMiniAPI.DB.PizzaDB.RemovePizza(id));
+app.MapGet("/pizzas/{id}",async (PizzaDb db,int id)=>await db.Pizzas.FindAsync(id));
+app.MapGet("/pizzas",async (PizzaDb db)=>await db.Pizzas.ToListAsync());
+app.MapPost("/pizzas",async (PizzaDb db,Pizza pizza) =>{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}",pizza);
+
+});
+app.MapPut("/pizzas",async (PizzaDb db,Pizza updatePizaa,int id) =>
+{
+    var pizza=await db.Pizzas.FindAsync(id);
+    if(pizza is null) return Results.NotFound();
+    pizza.Name=updatePizaa.Name;
+    pizza.Description=updatePizaa.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/pizzas/{id}",async (PizzaDb db,int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if(pizza is null) return Results.NotFound();
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
 
 app.Run();
